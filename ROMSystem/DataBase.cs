04ebba20b8df648b33da8faed8b11a45a9959456
商 黄 <tomdanhell@outlook.com>
2025-02-10 14:29:38 +0800

    添加项目文件。

---
+++ ROMSystem/DataBase.cs
@@ -0,0 +1,25 @@
+﻿using SqlSugar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ROMSystem
+{
+    public static class DataBase
+    {
+        public static string ConnectionString = @"DataSource=ROM_SQL.sqlite";
+
+        public static SqlSugarClient GetDB()
+        {
+            var db = new SqlSugarClient(new ConnectionConfig()
+            {
+                DbType = SqlSugar.DbType.Sqlite,
+                ConnectionString = ConnectionString,
+                IsAutoCloseConnection = true,
+            });
+            return db
+        }
+    }
+}
